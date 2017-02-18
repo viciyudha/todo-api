931f66fc0d4b3a9ac31905f33cf6faacbe9ba142
Arya Viciyudha <arya.rezavidi@gmail.com>
2017-02-18 14:00:40 +0700

    Add DELETE method

--- server.js
+++ server.js
@@ -45,6 +45,21 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		res.status(404).json({"error": "No todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);	
+		res.json(matchedTodo);
+	}
+		
+}); 
+
+
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
 });
