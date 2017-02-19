e9e594d3685ec4f4debeab9dbbe650b23b245a79
Arya Viciyudha <arya.rezavidi@gmail.com>
2017-02-19 12:36:23 +0700

    Add filer to GET /todos

--- server.js
+++ server.js
@@ -13,8 +13,19 @@ app.get('/', function (req, res) {
 	res.send('To Do API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {
