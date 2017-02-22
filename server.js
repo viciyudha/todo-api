86a5469c1ca2afc75ef0ce07e66b39e871875851
Arya Viciyudha <arya.rezavidi@gmail.com>
2017-02-22 22:56:51 +0700

    Add sequelize for DELETE /todos/:id

--- server.js
+++ server.js
@@ -104,14 +104,31 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (!matchedTodo) {
-		res.status(404).json({"error": "No todo found with that id"});
-	} else {
-		todos = _.without(todos, matchedTodo);	
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with ID'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
+
+	// var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({"error": "No todo found with that id"});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo);	
+	// 	res.json(matchedTodo);
+	// }
 		
 }); 
 
